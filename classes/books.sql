a9856a409e9db26b6e391195a985fbfaeb0e26a2
AlexSalako <alex.salako26@gmail.com>
2018-06-22 11:14:50 +0100

    Add files via upload

---
+++ classes/books.sql
@@ -0,0 +1,3 @@
+INSERT INTO BOOK (id, bookTitle, genre, yearPublished) VALUES (1234, 'Harry Potter and the Philosopher's Stone', 'Fantasy', 1997)
+INSERT INTO BOOK (id, bookTitle, genre, yearPublished) VALUES (1235, 'Cat in the Hat', 'Children Literature', 1957)
+INSERT INTO BOOK (id, bookTitle, genre, yearPublished) VALUES (1236, 'Mein Kampf', 'Autobiography', 1925)
